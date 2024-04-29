d3ac07e55bfd00181e550c4c04f69f31623af19e
AllineMorais <153544392+AllineMorais@users.noreply.github.com>
2024-04-28 21:26:25 -0300

    Initial commit

---
+++ Orgs/app/src/main/java/com/example/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Boas-vindas", Toast.LENGTH_SHORT).show()
+    }
+}
